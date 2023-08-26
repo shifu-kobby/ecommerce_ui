003239ec78753e6867c51ab489d997abee70ef2f
shifu-kobby <samuelarmahappiah@gmail.com>
2023-08-26 20:27:53 +0000

    adjust image size

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '@/lib/client';
@@ -6,24 +6,26 @@ import { Product } from '@/components';
 
 const ProductDetails = ({ product, products }: any) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
+
     return (
         <div>
             <div className='product-detail-container'>
                 <div>
                     <div className='image-container'>
-                        <img src={urlFor(image && image[0])} />
+                        <img src={urlFor(image && image[index])} className='product-detail-image'/>
                     </div>
-                    {/* <div className='small-images-container'>
+                    <div className='small-images-container'>
                         {
-                            image?.map((item, i) => (
+                            image?.map((item: any, i: any) => (
                                 <img 
                                     src={urlFor(item)}
-                                    className={''}
-                                    onMouseEnter={''}
+                                    className={i === index ? 'small-image selected-image': 'small-image'}
+                                    onMouseEnter={() => setIndex(i)}
                                 />
                             ))
                         }
-                    </div> */}
+                    </div>
                 </div>
 
                 <div className='product-detail-desc'>
