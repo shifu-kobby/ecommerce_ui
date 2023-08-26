48102cf892d561cc6fd60048cbcdc52ed5c9d439
shifu-kobby <samuelarmahappiah@gmail.com>
2023-08-26 20:14:10 +0000

    implement other products carousel

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '@/lib/client';
+import { Product } from '@/components';
 
 const ProductDetails = ({ product, products }: any) => {
     const { image, name, details, price } = product;
@@ -56,6 +57,19 @@ const ProductDetails = ({ product, products }: any) => {
                     </div>
                 </div>
             </div>
+
+            <div className='maylike-products-wrapper'>
+                <h2>You may also like</h2>
+                <div className='marquee'>
+                    <div className='maylike-products-container track'>
+                        {
+                            products.map((item: any) => (
+                                <Product key={item._id} product={item} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -86,9 +100,9 @@ export const getStaticPaths = async () => {
     }))
 
     return {
-      paths,
-      fallback: 'blocking'
+        paths,
+        fallback: 'blocking'
     }
-  }
+}
 
 export default ProductDetails;
