950efd7488c988d9f62cde091b608a47ae0da90f
shifu-kobby <samuelarmahappiah@gmail.com>
2023-08-26 19:03:56 +0000

    set product page routing

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { client, urlFor } from '@/lib/client';
+
+const ProductDetails = ({ product, products }: any) => {
+    const { image, name, details, price } = product;
+    return (
+        <div>
+            <div className='product-detail-container'>
+                <div>
+                    <div className='image-container'>
+                        <img src={urlFor(image && image[0])} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const getStaticProps = async ({ params: { slug } }: any) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]'
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+
+    return {
+        props: { product, products }
+    }
+}
+
+export const getStaticPaths = async ({ slug }: any) => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`
+    const products = await client.fetch(query);
+    const paths = products.map((product: any) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }))
+
+    return {
+      paths,
+      fallback: 'blocking'
+    }
+  }
+
+export default ProductDetails;
