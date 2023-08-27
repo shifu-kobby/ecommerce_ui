ae9ff251c1261b78aa098304fcfcebe0ef2aab79
shifu-kobby <samuelarmahappiah@gmail.com>
2023-08-27 13:41:24 +0000

    set up react context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import { Layout } from '@/components';
+import { StateContext } from '@/context/StateContext';
+import { Toaster } from 'react-hot-toast';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
